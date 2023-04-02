5f78b2345bc8d80a2672127a9c70a8f6e6864747
haronxbelghit <haronbel@gmail.com>
2023-04-02 21:43:40 +0000

    Initial Commit

---
+++ SimpleTodoApp/SimpleTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoAppApp.swift
+//  SimpleTodoApp
+//
+//  Created by Belghit Haron on 2/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
